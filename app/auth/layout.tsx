abadc7d758c8919a22dd0220f368a180974c2358
Szymon Tymcio <s.tymcio98@gmail.com>
2024-04-22 07:27:50 +0200

    modfy layout auth

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,13 +1,9 @@
-import { Box, Flex } from "@radix-ui/themes";
-import React, { PropsWithChildren } from "react";
-import Socials from "@/components/auth/Socials";
-import Header from "@/components/auth/Header";
+import { PropsWithChildren } from "react";
 
 const layout = ({ children }: PropsWithChildren) => {
 	return (
 		<div className='h-screen flex items-center justify-center bg-sky-200'>
 			{children}
-			{/* <Socials /> */}
 		</div>
 	);
 };
