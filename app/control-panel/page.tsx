248071e4f9fd45fb4559d761fc186aca8a484c33
Szymon Tymcio <s.tymcio98@gmail.com>
2024-06-02 13:04:57 +0200

    ui/ux control-panel and new-action

--- app/control-panel/page.tsx
+++ app/control-panel/page.tsx
@@ -1,12 +1,10 @@
 "use client";
+import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
-import { useUserByAuth } from "@/lib/services/queries";
-import { Metadata } from "next";
 import { columns } from "./components/columns";
 import { DataTable } from "./components/data-table";
+import LoadingPanelPage from "./components/spinner-page";
 import { useOrganizationEntitiesActions } from "./data/query";
-import { useSession } from "next-auth/react";
-import Image from "next/image";
 
 export default function DemoPage() {
 	const { data: session, status } = useSession();
@@ -27,23 +25,8 @@ export default function DemoPage() {
 
 	return (
 		<>
-			<div className='md:hidden'>
-				{/* <Image
-					src=''
-					width={1280}
-					height={998}
-					alt='Playground'
-					className='block dark:hidden'
-				/>
-				<Image
-					src=''
-					width={1280}
-					height={998}
-					alt='Playground'
-					className='hidden dark:block'
-				/> */}
-			</div>
-			<div className='hidden h-full flex-1 flex-col space-y-8 p-8 md:flex'>
+			<div className='md:hidden'></div>
+			<div className='hidden h-full flex-1 flex-col space-y-4 p-8 md:flex'>
 				<div className='flex items-center justify-between space-y-2'>
 					<div>
 						<h2 className='text-2xl font-bold tracking-tight'>Welcome back!</h2>
@@ -53,7 +36,12 @@ export default function DemoPage() {
 					</div>
 					<div className='flex items-center space-x-2'>{/* <UserNav /> */}</div>
 				</div>
-				{actions && <DataTable data={actions} columns={columns} />}
+				{actions ? (
+					<DataTable data={actions} columns={columns} />
+				) : (
+					<LoadingPanelPage />
+				)}
+				{/* <LoadingPanelPage /> */}
 			</div>
 		</>
 	);
